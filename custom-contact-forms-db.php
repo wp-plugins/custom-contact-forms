255bacc4d757ab02b10ef38ef278b7ca875f3f8a
tlovett1 <tlovett1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-19 23:21:24 +0000

    git-svn-id: https://plugins.svn.wordpress.org/custom-contact-forms/tags/2.2.5@278495 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ custom-contact-forms-db.php
@@ -0,0 +1,320 @@
+<?php
+/*
+	Custom Contact Forms Plugin
+	By Taylor Lovett - http://www.taylorlovett.com
+	Plugin URL: http://www.taylorlovett.com/wordpress-plugins
+*/
+if (!class_exists('CustomContactFormsDB')) {
+	class CustomContactFormsDB {
+		var $forms_table;
+		var $fields_table;
+		var $styles_table;
+		function CustomContactFormsDB() {
+			global $wpdb;
+			$this->forms_table = $wpdb->prefix . 'customcontactforms_forms';
+			$this->fields_table = $wpdb->prefix . 'customcontactforms_fields';
+			$this->styles_table = $wpdb->prefix . 'customcontactforms_styles';
+			$this->createTables();
+			$this->updateTables();
+			$this->insertFixedFields();
+		}
+		
+		function encodeOption($option) {
+			return htmlspecialchars(stripslashes($option), ENT_QUOTES);
+		}
+		
+		function decodeOption($option, $strip_slashes = 1, $decode_html_chars = 1) {
+			if ($strip_slashes == 1) $option = stripslashes($option);
+			if ($decode_html_chars == 1) $option = html_entity_decode($option);
+			return $option;
+		}
+		
+		function createTables() {
+			global $wpdb;
+			if(!$this->formsTableExists()) {
+				$sql1 = " CREATE TABLE `".$this->forms_table."` (
+						`id` INT( 11 ) NOT NULL AUTO_INCREMENT ,
+						`form_slug` VARCHAR( 100 ) NOT NULL ,
+						`form_title` VARCHAR( 200 ) NOT NULL ,
+						`form_action` TEXT NOT NULL ,
+						`form_method` VARCHAR( 4 ) NOT NULL ,
+						`form_fields` VARCHAR( 200 ) NOT NULL ,
+						`submit_button_text` VARCHAR( 200 ) NOT NULL ,
+						`custom_code` TEXT NOT NULL ,
+						PRIMARY KEY ( `id` )
+						) ENGINE = MYISAM AUTO_INCREMENT=1 ";
+				$wpdb->query($sql1);
+			} if(!$this->fieldsTableExists()) {
+				$sql2 = "CREATE TABLE `".$this->fields_table."` (
+						`id` INT( 11 ) NOT NULL AUTO_INCREMENT ,
+						`field_slug` VARCHAR( 50 ) NOT NULL ,
+						`field_label` VARCHAR( 100 ) NOT NULL ,
+						`field_type` VARCHAR( 25 ) NOT NULL ,
+						`field_value` TEXT NOT NULL ,
+						`field_maxlength` INT ( 5 )  NOT NULL DEFAULT '0',
+						`user_field` INT ( 1 )  NOT NULL DEFAULT '1',
+						PRIMARY KEY ( `id` )
+						) ENGINE = MYISAM AUTO_INCREMENT=1 ";
+				$wpdb->query($sql2);
+			} if(!$this->stylesTableExists()) {
+				// Title, input, textarea, label, form, submit
+				$sql3 = "CREATE TABLE `".$this->styles_table."` (
+						`id` INT( 11 ) NOT NULL AUTO_INCREMENT ,
+						`style_slug` VARCHAR( 30 ) NOT NULL ,
+						`input_width` VARCHAR( 10 ) NOT NULL DEFAULT '200px',
+						`textarea_width` VARCHAR( 10 ) NOT NULL DEFAULT '200px',
+						`textarea_height` VARCHAR( 10 ) NOT NULL DEFAULT '100px',
+						`form_borderwidth` VARCHAR( 10 ) NOT NULL DEFAULT '0px',
+						`label_width` VARCHAR( 10 ) NOT NULL DEFAULT '110px',
+						`form_width` VARCHAR( 10 ) NOT NULL DEFAULT '500px',
+						`submit_width` VARCHAR( 10 ) NOT NULL DEFAULT '80px',
+						`submit_height` VARCHAR( 10 ) NOT NULL DEFAULT '35px',
+						`label_fontsize` VARCHAR( 10 ) NOT NULL DEFAULT '1em',
+						`title_fontsize` VARCHAR( 10 ) NOT NULL DEFAULT '1.2em',
+						`field_fontsize` VARCHAR( 10 ) NOT NULL DEFAULT '1em',
+						`submit_fontsize` VARCHAR( 10 ) NOT NULL DEFAULT '1.1em',
+						`field_bordercolor` VARCHAR( 10 ) NOT NULL DEFAULT 'blue',
+						`form_borderstyle` VARCHAR( 30 ) NOT NULL DEFAULT 'dashed',
+						`form_bordercolor` VARCHAR( 20 ) NOT NULL DEFAULT 'black',
+						`field_fontcolor` VARCHAR( 20 ) NOT NULL DEFAULT '#000000',
+						`label_fontcolor` VARCHAR( 20 ) NOT NULL DEFAULT '#333333',
+						`title_fontcolor` VARCHAR( 20 ) NOT NULL DEFAULT '#333333',
+						`submit_fontcolor` VARCHAR( 20 ) NOT NULL DEFAULT '#333333',
+						`form_fontfamily` VARCHAR( 150 ) NOT NULL DEFAULT 'Verdana, Tahoma, Arial',
+						PRIMARY KEY ( `id` )
+						) ENGINE = MYISAM AUTO_INCREMENT=1 ";
+				$wpdb->query($sql3);
+			}
+			return true;
+		}
+		
+		function formatStyle($style) {
+			return str_replace(';', '', $style);
+		}
+		
+		function updateTables() {
+			global $wpdb;
+			if (!$this->columnExists('user_field', $this->fields_table))
+				$wpdb->query("ALTER TABLE `" . $this->fields_table . "` ADD `user_field` INT( 1 ) NOT NULL DEFAULT '1'");
+			if (!$this->columnExists('form_style', $this->forms_table))
+				$wpdb->query("ALTER TABLE `" . $this->forms_table . "` ADD `form_style` INT( 10 ) NOT NULL DEFAULT '0'");
+			if (!$this->columnExists('field_backgroundcolor', $this->styles_table))
+				$wpdb->query("ALTER TABLE `" . $this->styles_table . "` ADD `field_backgroundcolor` VARCHAR( 20 ) NOT NULL DEFAULT '#efefef'");
+			if (!$this->columnExists('field_borderstyle', $this->styles_table))
+				$wpdb->query("ALTER TABLE `" . $this->styles_table . "` ADD `field_borderstyle` VARCHAR( 20 ) NOT NULL DEFAULT 'solid'");
+			if (!$this->columnExists('form_margin', $this->styles_table))
+				$wpdb->query("ALTER TABLE `" . $this->styles_table . "` ADD `form_margin` VARCHAR( 20 ) NOT NULL DEFAULT '6px'");
+			if (!$this->columnExists('form_padding', $this->styles_table))
+				$wpdb->query("ALTER TABLE `" . $this->styles_table . "` ADD `form_padding` VARCHAR( 20 ) NOT NULL DEFAULT '4px'");
+			if (!$this->columnExists('title_margin', $this->styles_table))
+				$wpdb->query("ALTER TABLE `" . $this->styles_table . "` ADD `title_margin` VARCHAR( 20 ) NOT NULL DEFAULT '2px'");
+			if (!$this->columnExists('label_margin', $this->styles_table))
+				$wpdb->query("ALTER TABLE `" . $this->styles_table . "` ADD `label_margin` VARCHAR( 20 ) NOT NULL DEFAULT '3px'");
+			if (!$this->columnExists('field_instructions', $this->fields_table))
+				$wpdb->query("ALTER TABLE `" . $this->fields_table . "` ADD `field_instructions` TEXT NOT NULL");
+		}
+		
+		function insertFixedFields() {
+			$captcha = array('field_slug' => 'captcha', 'field_label' => 'Type the numbers.', 'field_type' => 'Text', 'field_value' => '', 'field_maxlength' => '100', 'user_field' => 0, 'field_instructions' => 'Type the numbers displayed in the image above.');
+			$ishuman = array('field_slug' => 'ishuman', 'field_label' => 'Check if you are human.', 'field_type' => 'Checkbox', 'field_value' => '1', 'field_maxlength' => '0', 'user_field' => 0, 'field_instructions' => 'This helps us prevent spam.');
+			$fixedEmail = array('field_slug' => 'fixedEmail', 'field_label' => 'Your Email', 'field_type' => 'Text', 'field_value' => '', 'field_maxlength' => '100', 'user_field' => 0, 'field_instructions' => 'Please enter your email address.');
+			if (!$this->fieldSlugExists('captcha'))
+				$this->insertField($captcha);
+			if (!$this->fieldSlugExists('ishuman'))
+				$this->insertField($ishuman);
+			if (!$this->fieldSlugExists('fixedEmail'))
+				$this->insertField($fixedEmail);
+		
+		}
+		
+		function columnExists($column, $table) {
+			global $wpdb;
+			$tests = $wpdb->get_results("SELECT * FROM INFORMATION_SCHEMA.columns WHERE table_name = '$table' AND column_name = '$column' LIMIT 0 , 30");
+			return (!empty($test[0]) && $test[0]->COLUMN_NAME == $column);
+		}
+		
+		function insertForm($form_slug, $form_title, $form_action, $form_method, $submit_button_text, $custom_code, $form_style) {
+			global $wpdb;
+			if ($this->formSlugExists($this->formatSlug($form_slug))) return false;
+			$wpdb->insert($this->forms_table, array('form_slug' => $this->formatSlug($form_slug), 'form_title' => $this->encodeOption($form_title), 'form_action' => $this->encodeOption($form_action), 'form_method' => $form_method, 'submit_button_text' => $this->encodeOption($submit_button_text), 'form_style' => $form_style, 'custom_code' => $this->encodeOption($custom_code)));
+			return true;
+		}
+		
+		function insertField($field) {
+			global $wpdb;
+			if (empty($field) or empty($field[field_slug]) or $this->fieldSlugExists($this->formatSlug($field[field_slug]))) return false;
+			$field[field_slug] = $this->formatSlug($field[field_slug]);
+			foreach ($field as $key => $value) {
+				if ($key != 'field_slug')
+					$field[$key] = $this->encodeOption($value);
+			}
+			$wpdb->insert($this->fields_table, $field);
+			return true;
+		}
+		
+		function insertStyle($style) {
+			global $wpdb;
+			if (empty($style) or empty($style[style_slug]) or $this->styleSlugExists($this->formatSlug($style[style_slug]))) return false;
+			$style[style_slug] = $this->formatSlug($style[style_slug]);
+			foreach ($style as $key => $value) {
+				if ($key != 'style_slug')
+					$style[$key] = $this->formatStyle($this->encodeOption($value));
+			}
+			$wpdb->insert($this->styles_table, $style);
+			return true;
+		}
+		
+		function fieldsTableExists() {
+			global $wpdb;
+			return ($wpdb->get_var("show tables like '". $this->fields_table . "'") == $this->fields_table);
+		}
+		
+		function formsTableExists() {
+			global $wpdb;
+			return ($wpdb->get_var("show tables like '". $this->forms_table . "'") == $this->forms_table);
+		}
+		
+		function stylesTableExists() {
+			global $wpdb;
+			return ($wpdb->get_var("show tables like '". $this->styles_table . "'") == $this->styles_table);
+		}
+		
+		function updateForm($form_slug, $form_title, $form_action, $form_method, $submit_button_text, $custom_code, $form_style, $fid) {
+			global $wpdb;
+			if (empty($form_slug)) return false;
+			$test = $this->selectForm('', $form_slug);
+			if (!empty($test) and $test->id != $fid) // if form_slug is different then make sure it is unique
+				return false;
+			$wpdb->update($this->forms_table, array('form_slug' => $this->formatSlug($form_slug), 'form_title' => $this->encodeOption($form_title), 'form_action' => $this->encodeOption($form_action), 'form_method' => $form_method, 'form_style' => $form_style, 'submit_button_text' => $this->encodeOption($submit_button_text), 'custom_code' => $this->encodeOption($custom_code)), array('id' => $fid));
+			return true;
+		}
+		
+		function updateField($field_slug, $field_label, $field_type, $field_value, $field_maxlength, $field_instructions, $fid) {
+			global $wpdb;
+			if (empty($field_slug)) return false;
+			$test = $this->selectField('', $field_slug);
+			if (!empty($test) and $test->id != $fid) // if field_slug is different then make sure it is unique
+				return false;
+			$wpdb->update($this->fields_table, array('field_slug' => $this->formatSlug($field_slug), 'field_label' => $this->encodeOption($field_label), 'field_type' => $field_type, 'field_value' => $this->encodeOption($field_value), 'field_maxlength' => $this->encodeOption($field_maxlength), 'field_instructions' => $this->encodeOption($field_instructions)), array('id' => $fid));
+			return true;
+		}
+		
+		function updateStyle($style) {
+			global $wpdb;
+			if (empty($style[style_slug])) return false;
+			$test = $this->selectStyle('', $style[style_slug]);
+			if (!empty($test) and $test->id != $style[id]) // if style_slug is different then make sure it is unique
+				return false;
+			$style[style_slug] = $this->formatSlug($style[style_slug]);
+			foreach ($style as $key => $value) {
+				if ($key != 'style_slug')
+					$style[$key] = $this->formatStyle($this->encodeOption($value));
+			}
+			$wpdb->update($this->styles_table, $style, array('id' => $style[id]));
+			return true;
+		}
+		
+		function deleteForm($fid) {
+			global $wpdb;
+			$wpdb->query("DELETE FROM " . $this->forms_table . " WHERE id='$fid'");
+			return true;
+		}
+		
+		function deleteField($fid) {
+			global $wpdb;
+			$wpdb->query("DELETE FROM " . $this->fields_table . " WHERE id='$fid'");
+			return true;
+		}
+		
+		function deleteStyle($sid) {
+			global $wpdb;
+			$wpdb->query("DELETE FROM " . $this->styles_table . " WHERE id='$sid'");
+			return true;
+		}
+		
+		function selectAllForms() {
+			global $wpdb;
+			return $wpdb->get_results("SELECT * FROM " . $this->forms_table . " ORDER BY form_slug ASC");	
+		}
+		
+		function selectAllFields() {
+			global $wpdb;
+			return $wpdb->get_results("SELECT * FROM " . $this->fields_table . " ORDER BY field_slug ASC");	
+		}
+		
+		function selectAllStyles() {
+			global $wpdb;
+			return $wpdb->get_results("SELECT * FROM " . $this->styles_table . " ORDER BY style_slug ASC");	
+		}
+		
+		function selectForm($fid, $form_slug) {
+			global $wpdb;
+			$extra = (!empty($form_slug)) ? " or form_slug = '$form_slug'" : '';
+			return $wpdb->get_row("SELECT * FROM " . $this->forms_table . " WHERE id='$fid' $extra");
+		}
+		
+		function selectStyle($sid, $style_slug) {
+			global $wpdb;
+			$extra = (!empty($style_slug)) ? " or style_slug = '$style_slug'" : '';
+			return $wpdb->get_row("SELECT * FROM " . $this->styles_table . " WHERE id='$sid' $extra");
+		}
+		
+		function selectField($fid, $field_slug) {
+			global $wpdb;
+			$extra = (!empty($field_slug)) ? " or field_slug = '$field_slug'" : '';
+			return $wpdb->get_row("SELECT * FROM " . $this->fields_table . " WHERE id='$fid'" . $extra);
+		}
+		
+		function addFieldToForm($field_id, $form_id) {
+			global $wpdb;
+			$form = $this->selectForm($form_id, '');
+			$fields = $this->getAttachedFieldsArray($form_id);
+			if (!in_array($field_id, $fields)) {
+				$newfields = $form->form_fields . $field_id . ',';
+				$wpdb->update($this->forms_table, array('form_fields' => $newfields), array('id' => $form_id));
+				return true;
+			}
+			return false;
+		}
+		
+		function getAttachedFieldsArray($form_id) {
+			$form = $this->selectForm($form_id, '');
+			$out = explode(',', $form->form_fields);
+			if (!empty($out)) array_pop($out);
+			return $out;
+		}
+		
+		function disattachField($field_id, $form_id) {
+			global $wpdb;
+			$fields = $this->getAttachedFieldsArray($form_id);
+			if (!empty($fields) && in_array($field_id, $fields)) {
+				$form = $this->selectForm($form_id, '');
+				$newfields = str_replace($field_id . ',', '', $form->form_fields);
+				$wpdb->update($this->forms_table, array('form_fields' => $newfields), array('id' => $form_id));
+				return true;
+			}
+			return false;
+		}
+		
+		function formatSlug($slug) {
+			$slug = preg_replace('/[^a-zA-Z0-9\s]/', '', $slug);
+			return str_replace(' ', '_', $slug);	
+		}
+		
+		function fieldSlugExists($slug) {
+			$test = $this->selectField('', $slug);
+			return (!empty($test));
+		}
+		
+		function styleSlugExists($slug) {
+			$test = $this->selectStyle('', $slug);
+			return (!empty($test));
+		}
+		
+		function formSlugExists($slug) {
+			$test = $this->selectForm('', $slug);
+			return (!empty($test));
+		}
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,148 @@
+=== Custom Contact Forms ===
+Plugin Name: Custom Contact Forms
+Tags: contact form, web form, custom contact form, custom forms, captcha form, contact fields, form mailers
+Requires at least: 2.7.1
+Tested up to: 3.1
+Stable tag: 2.2.5
+Contributors: Taylor Lovett
+Download link: http://www.taylorlovett.com/wordpress-plugins/
+Author: Taylor Lovett
+Author URI: http://www.taylorlovett.com
+
+Gauranteed the most customizable contact form plugin form for Wordpress.
+
+== Description ==
+
+Guaranteed to be 1000X more customizable and intuitive than Fast Secure Contact Forms or Contact Form 7. CUSTOMIZE EVERY ASPECT OF YOUR FORMS APPEARANCE WITH ANY EASY TO USE FORM - BORDERS, FONT SIZES, COLORS, PADDING, MARGINS, BACKGROUNDS, AND MORE. A plugin for handling and displaying custom web forms in any page, post, or sidebar.
+
+Special Features:
+------------------
+
+* The most customizable form plugin for Wordpress, guaranteed
+
+* Create unlimited forms
+
+* Create unlimited fields
+
+* Customize every aspect of fields and forms: titles, labels, maxlength, initial value, form action, form method, form style, and much more
+
+* Create checkboxes, textareas, text fields, etc.
+
+* Captcha and "Are You Human?" spam blockers included and easily attached to any form
+
+* Create custom styles to change the appearance of your forms: borders, font sizes, colors, padding, margins, background, and more
+
+* You can create unlimited styles to use on as many forms as you want - NO CSS KNOWLEDGE REQUIRED!
+
+* Show a stylish JQuery form thank you message or use a custom thank you page.
+
+* Custom error pages for when forms are filled out incorrectly
+
+* Option to have forms remember field values for when users hit the back button after an error
+
+* Easily report bugs and suggest new features
+
+* Script in constant development - new version released every week
+
+* Easily process your forms with 3rd party sites like Infusionsoft or Aweber
+
+* Valid XHTML and CSS
+
+* No javascript required
+
+* Stylish field tooltips powered by jquery
+
+* Popover forms with Jquery (Coming soon!)
+
+* Free unlimited support
+
+== Installation ==
+1. Upload to /wp-content/plugins
+2. Activate the plugin from your Wordpress Admin Panel
+3. Configure the plugin, create fields, and create forms in the Settings page called Custom Contact Forms
+4. Display those forms in posts and pages by inserting the code: [customcontact form=FORMID]
+
+== Configuring and Using the Plugin ==
+1. Create a form in the Custom Contact Forms setting page. To get to the settings page, click the Custom Contact Forms link in the admin panel sidebar
+2. Create fields and attach those fields to the forms of your choice. * Attach the fields in the order that you want them to show up in the form. If you mess up you can detach and reattach them.
+3. Display those forms in posts and pages by inserting the code: [customcontact form=FORMID]. Replace FORMID with the id listed to the left of the form slug next to the form of your choice above.
+4. Add a form to your sidebar, by dragging the Custom Contact Form widget in to your sidebar.
+5. Configure the General Settings appropriately; this is important if you want to receive your web form messages!
+
+== Support ==
+For questions, feature requests, and support concerning the Custom Contact Forms plugin, please email me at:
+admin@taylorlovett.com
+I respond to emails same-day!
+
+== Upgrade Notice ==
+Popover forms will be added in September 2010.
+
+== Change Log ==
+
+1.0.0:
+* Plugin Release
+
+1.0.1:
+* custom-contact-forms.css - Form style changes
+
+1.1.0:
+* custom-contact-forms-db.php - Table upgrade functions added
+* custom-contact-forms.php - New functions for error handling and captcha
+* custom-contact-forms.css - Forms restyled
+* custom-contact-forms-images.php - Image handling class added
+* image.php, images/ - Image for captcha displaying
+
+1.1.1
+* custom-contact-forms.css - Label styles changed
+* custom-contact-forms.php - Admin option added to remember field values
+
+1.1.2
+* custom-contact-forms-db.php - create_tables function edited to work for Wordpress MU due to error in wp-admin/includes/upgrade.php
+
+1.1.3
+* custom-contact-forms.php - Captcha label bug fixed
+* custom-contact-forms-db.php - Default captcha label changed
+
+1.2.0
+* custom-contact-forms.php - Option to update to Custom Contact Forms Pro
+
+1.2.1
+* custom-contact-forms.php - Upgrade options changed
+* custom-contact-forms-css.php - CSS bug corrected
+
+2.0.0
+* custom-contact-forms.php - Style manager added
+* custom-contact-forms.css - style manager styles added
+* custom-contact-forms-db.php - Style manager db functions added
+
+2.0.1
+* custom-contact-forms.php - Duplicate form slug bug fixed, default style values added, stripslahses on form messages
+* custom-contact-forms-db.php - default style values added
+
+2.0.2
+* custom-contact-forms.php - Form li's changed to p's
+* images/ - folder readded to correct captcha error
+
+2.0.3
+* custom-contact-forms.php - custom style checkbox display:block error fixed
+* custom-contact-forms.css - li's converted to p's
+
+2.1.0
+* custom-contact-forms.php - New fixed field added, plugin news, bug fixes
+* custom-contact-forms.css - New styles added and style bugs fixed
+* custom-contact-forms-db.php - New fixed field added
+
+2.2.0
+* custom-contact-forms.php - Plugin nav, hide plugin author link, bug reporting, suggest a feature
+* custom-contact-forms.css - New styles added and style bugs fixed
+
+2.2.3
+* custom-contact-forms.php - Remember fields bug fixed, init rearranged, field instructions
+* custom-contact-forms.css
+* custom-contact-forms-db.php
+
+2.2.4
+* custom-contact-forms.php - Textarea field instruction bug fixed
+
+2.2.5
+* custom-contact-forms.php - Fixed field insert bug fixed
