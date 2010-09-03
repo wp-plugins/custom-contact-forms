f7437eca6bf155449d0c0e43f225fece9bd55773
tlovett1 <tlovett1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-03 16:33:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/custom-contact-forms/tags/3.5.0@284609 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/custom-contact-forms-admin.js
@@ -0,0 +1,39 @@
+var $j = jQuery.noConflict();
+//alert('shit');
+$j(document).ready(function(){
+	$j('.form-extra-options').hide();
+	$j('.form-options-expand').prepend('<input type="button" class="form-options-expand-link" value="More Options" />');
+	$j('.form-options-expand-link').click(function() {
+		$j(this)
+			.parent()
+			.parent()
+			.parent()
+			.next()
+			.find(".form-extra-options:first")
+			.toggle();
+	});
+	
+	$j('.fixed-fields-extra-options').hide();
+	$j('.fixed-fields-options-expand').prepend('<input type="button" class="fixed-fields-options-expand-link" value="More Options" />');
+	$j('.fixed-fields-options-expand-link').click(function() {
+		$j(this)
+			.parent()
+			.parent()
+			.parent()
+			.next()
+			.find(".fixed-fields-extra-options:first")
+			.toggle();
+	});
+	
+	$j('.fields-extra-options').hide();
+	$j('.fields-options-expand').prepend('<input type="button" class="fields-options-expand-link" value="More Options" />');
+	$j('.fields-options-expand-link').click(function() {
+		$j(this)
+			.parent()
+			.parent()
+			.parent()
+			.next()
+			.find(".fields-extra-options:first")
+			.toggle();
+	});
+});
