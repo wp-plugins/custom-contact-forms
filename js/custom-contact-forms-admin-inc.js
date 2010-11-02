5a2a63ec7f55c5a46fbacb01fbb825829d80e8dc
tlovett1 <tlovett1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-02 15:41:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/custom-contact-forms/tags/4.0.0.b9@306978 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/custom-contact-forms-admin-inc.js
@@ -0,0 +1,61 @@
+var $j = jQuery.noConflict();
+
+function showCCFUsagePopover() {
+	$j("#ccf-usage-popover").fadeIn('slow');	
+}
+
+(function($j) {
+  var cache = [];
+  // Arguments are image paths relative to the current page.
+  $j.preloadImages = function() {
+    var args_len = arguments.length;
+    for (var i = args_len; i--;) {
+      var cacheImage = document.createElement('img');
+      cacheImage.src = arguments[i];
+      cache.push(cacheImage);
+    }
+  }
+})(jQuery)
+
+var fx = {
+	"initModal" : function() {
+		if ($j(".modal-window").length == 0) {
+			return $j("<div>")
+				.addClass("modal-window")
+				.appendTo("body");
+		} else {
+			return $j(".modal-window");
+		}
+	},
+	
+	"initDebugWindow" : function() {
+		if ($j(".debug-window").length == 0) {
+			debug = $j("<div>").addClass("debug-window").appendTo("body");
+			debug.click(function() { debug.remove(); });
+			return debug;
+		} else {
+			return $j(".debug-window");
+		}
+	},
+	
+	"initSaveBox" : function(text) {
+		if ($j(".save-box").length == 0) {
+			box = $j("<div>").addClass("save-box").appendTo("body");
+			$j("<a>")
+				.attr("href", "#")
+				.addClass("save-box-close-btn")
+				.html("&times;")
+				.click(function(event) { event.preventDefault(); $j(".save-box").fadeOut("slow"); })
+				.appendTo(box);
+			$j("<p>").html(text + ' <img src="' + ccf_plugin_dir + '/images/wpspin_light.gif" />').appendTo(".save-box");
+			return box;
+		} else {
+			return $j(".save-box");
+		}
+	},
+	
+	"boxOut": function(event) {
+		if (event != undefined) event.preventDefault();
+		$j(".modal-window").fadeOut("slow", function() { $j(this).remove(); });
+	}
+};
